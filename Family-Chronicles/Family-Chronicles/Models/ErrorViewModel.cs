510583a6dfecab8a4e8df8344b4bfd98796c26c9
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2022-01-31 18:05:35 +0100

    init

---
+++ Family-Chronicles/Family-Chronicles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Family_Chronicles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
