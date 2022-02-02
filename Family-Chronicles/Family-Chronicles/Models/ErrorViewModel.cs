ded2186d12f07c3834b831dd411e349c9b00bfcf
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2022-02-03 00:35:31 +0100

    Restructuring Setup

--- Family-Chronicles/Family-Chronicles/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Family_Chronicles.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
