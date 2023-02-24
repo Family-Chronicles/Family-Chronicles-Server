ece72d26810a3f8670b43e5a439283c2c07083b5
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2023-02-25 00:13:36 +0100

    project reinit as a node backend

--- Family-Chronicles/src/Helper/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Family_Chronicles_Backend.Helper;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-global using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
