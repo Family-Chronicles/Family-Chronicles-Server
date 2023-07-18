dd9c957e250efaea6618e7f8301be5707555750e
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2023-07-18 22:27:34 +0200

    polishing error Handler

--- src/core/error.core.ts
+++ src/core/error.core.ts
@@ -3,16 +3,19 @@ import { DatabaseService } from "../services/database.srvs.js";
 import { ErrorModel } from "../models/error.model.js";
 
 export default class GlobalErrorHandler {
-	private _database = DatabaseService.getInstance();
-	private _collectionName = "errors";
+	private _database: DatabaseService;
+	private _collectionName: string = "errors";
 
 	constructor() {
+		this._database = DatabaseService.getInstance();
 		this.initDatabase();
 
 		process.on("uncaughtException", (error: Error) => {
 			console.error("Uncaught Exception:");
 			console.error(util.inspect(error, false, null, true));
-			this.logErrorToDatabase(error);
+			this.logErrorToDatabase(error).catch(() => {
+				console.error("Failed to log error to the database");
+			});
 			process.exit(1);
 		});
 
@@ -25,44 +28,53 @@ export default class GlobalErrorHandler {
 					"Reason:",
 					util.inspect(reason, false, null, true)
 				);
-				this.logRejectionToDatabase(reason, promise);
+				this.logRejectionToDatabase(reason, promise).catch(() => {
+					console.error("Failed to log rejection to the database");
+				});
 				process.exit(1);
 			}
 		);
 	}
 
-	private logErrorToDatabase(error: Error): void {
+	private async logErrorToDatabase(error: Error): Promise<void> {
 		const errorDocument = new ErrorModel(
 			error.name,
 			error.message,
 			error.stack!,
 			new Date()
 		);
-		this._database.createDocument(this._collectionName, errorDocument);
+		await this._database.createDocument(
+			this._collectionName,
+			errorDocument
+		);
 	}
 
-	private logRejectionToDatabase(reason: any, promise: Promise<any>): void {
+	private async logRejectionToDatabase(
+		reason: any,
+		promise: Promise<any>
+	): Promise<void> {
 		const errorDocument = new ErrorModel(
 			"Unhandled Promise Rejection",
 			reason,
 			promise.toString(),
 			new Date()
 		);
-		this._database.createDocument(this._collectionName, errorDocument);
+		await this._database.createDocument(
+			this._collectionName,
+			errorDocument
+		);
 	}
 
-	private initDatabase(): void {
-		this._database
-			.createCollection(this._collectionName)
-			.then(() => {
-				console.log(
-					`Collection '${this._collectionName}' created successfully.`
-				);
-			})
-			.catch((error) => {
-				console.log(
-					`Failed to create collection '${this._collectionName}': ${error.message}`
-				);
-			});
+	private async initDatabase(): Promise<void> {
+		try {
+			await this._database.createCollection(this._collectionName);
+			console.log(
+				`Collection '${this._collectionName}' created successfully.`
+			);
+		} catch (error: any) {
+			console.log(
+				`Failed to create collection '${this._collectionName}': ${error.message}`
+			);
+		}
 	}
 }
